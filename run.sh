7c8a0181bc13f6b0d0e2d0637ba56a139990177c
Vuong Mai <maivanvuong@gmail.com>
2023-01-02 22:22:09 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -e
-wget -O coocaa.m3u8 http://m3u.in/HVdAHQEO
+curl http://m3u.in/HVdAHQEO --output coocaa.m3u8
 sed -i 's/append/default/g' coocaa.m3u8
 curl -L http://playlist.vthanhtivi.pw/ --output vthanh.m3u8
 sed -i 's/append/default/g' vthanh.m3u8
