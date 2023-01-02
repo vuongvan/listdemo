c7a94f3a3686d4c54372e3470bf1a73134cf27aa
Vuong Mai <maivanvuong@gmail.com>
2023-01-02 22:12:05 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash -e
+#!/bin/sh -e
 wget -O coocaa.m3u8 https://dl.dropboxusercontent.com/s/fhon6tzc3e20tis/%20%C3ƒ%EF%BF%BD%C3‚%C2%A3%C3ƒ%C2%AF%C3‚%C2%BF%C3‚%C2%BD%C3ƒ%EF%BF%BD%C3‚%C2%A4
 sed -i 's/append/default/g' coocaa.m3u8
 curl -L http://playlist.vthanhtivi.pw/ --output vthanh.m3u8
