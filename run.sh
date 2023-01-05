48203c261582ef4ef34cbd3b8996588c2c819b5a
Vuong Mai <maivanvuong@gmail.com>
2023-01-05 21:24:52 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
-curl http://m3u.in/HVdAHQEO --output coocaa.m3u8
-sed -i 's/append/default/g' coocaa.m3u8
+#curl http://m3u.in/HVdAHQEO --output coocaa.m3u8
+#sed -i 's/append/default/g' coocaa.m3u8
 curl https://trada.info/epg.xml --output epg.xml
 gzip epg.xml
-curl -L http://playlist.vthanhtivi.pw/ --output vthanh.m3u8
-sed -i 's/append/default/g' vthanh.m3u8
+#curl -L http://playlist.vthanhtivi.pw/ --output vthanh.m3u8
+#sed -i 's/append/default/g' vthanh.m3u8
