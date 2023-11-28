7e3d245473c3d0ecf5202ce08d01871bf118bfa5
Fei Yu <maivanvuong@gmail.com>
2023-11-28 21:47:26 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 wget https://lichphatsong.xyz/schedule/epg.xml.gz -O epg.xml.gz
-wget http://gg.gg/coocaa -O cc.m3u
+#wget http://gg.gg/coocaa -O cc.m3u
 #sed -i 's/append/default/g' coocaa.m3u
 #curl -L http://playlist.vthanhtivi.pw/ --output vthanh.m3u8
 #sed -i 's/append/default/g' vthanh.m3u8
